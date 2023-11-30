a6b2f47b7394892bf8741efd96d8ab353a5a2e99
Akshay <akshay.kamble@copods.co>
2023-11-30 16:08:33 +0530

    git commit updated sheet

--- src/commands/log.js
+++ src/commands/log.js
@@ -27,15 +27,22 @@ const command = {
     const sheets = google.sheets({ version: 'v4', auth })
 
     // Add data to Google Sheets
-    const spreadsheetId = 'your-spreadsheet-id' // Replace with your Spreadsheet ID
-    const range = 'Sheet1!A1' // Replace with your desired range
-
+    const spreadsheetId = '1bhPXHDXPlFxGxhRMa3AjZeTHvJTqDRqDTjER4R9-l9g' // Replace with your Spreadsheet ID
+    const range = 'Sheet1!A1:C10' // Replace with your desired range
+    const sheetData = commits.map((commit) => [
+      commit.date,
+      commit.author,
+      commit.commit,
+      commit.message,
+    ])
+    const headers = ['Date', 'Author', 'Commit', 'Message']
+    sheetData.unshift(headers)
     await sheets.spreadsheets.values.append({
       spreadsheetId,
       range,
       valueInputOption: 'USER_ENTERED',
       resource: {
-        values: [commits], // Add your data here
+        values: sheetData,
       },
     })
 
